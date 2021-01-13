7dff72b132ca2fe78d650c60eade1e7ddc2d66b4
Clementine <clementinenocton@hotmail.fr>
2021-01-13 16:00:50 +0100

    Adding some style

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      @my_association = Association.find_by(rna_id: current_user.rna)
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,23 +6,21 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @asso = helpers.get_rna_asso_by_rna(@user.rna)
-    if @user.save
-      if @asso
-        if @user.asso_name == @asso[:name]
+    if @asso
+      if @user.asso_name === @asso[:name]
+        @user.save
+        if @user.save
           @association = Association.new(name: @asso[:name], rna_id: @asso[:rna_id], official_id: @asso[:official_id], user_id: @user.id)
           @association.save
           session[:user_id] = @user.id
           redirect_to root_url, notice: "Thank you for signing up!"
-        else
-          @user.errors.full_messages.push("Le nom renseigné ne correspond pas au numéro RNA")
-          render "new"
         end
       else
-        @user.errors.full_messages.push("Le numero RNA renseigné n'est pas valide")
+        flash.now[:notice] = "Le nom renseigné ne correspond pas au numéro RNA"
         render "new"
       end
     else
-      @user.errors.full_messages.push("L'utilisateur n'a pas pu être enregistré, veuillez réessayer.")
+      flash.now[:notice] = "Le numero RNA renseigné n'est pas valide"
       render "new"
     end
   end
